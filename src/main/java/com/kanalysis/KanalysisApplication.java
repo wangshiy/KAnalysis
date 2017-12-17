418105226df94006c1a51d7e1f5ef6acbdd86e49
Shiyu Wang <wangshiyu@Shiyus-MacBook-Pro.local>
2017-12-17 00:12:53 -0800

    init spring sts

---
+++ src/main/java/com/kanalysis/KanalysisApplication.java
@@ -0,0 +1,12 @@
+package com.kanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanalysisApplication.class, args);
+	}
+}
